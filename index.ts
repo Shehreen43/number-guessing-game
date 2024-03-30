9b8be73b03beaa80dd00b568d739f9b66c68e4f4
Shehreen <160480886+Shehreen43@users.noreply.github.com>
2024-03-30 11:53:11 +0500

    my first number guessing game

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/evn node
+
+import inquirer from "inquirer";
+
+//1) computer will generate a random num
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answers = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please gues a number 1-10:",
+  },
+]);
+
+if (answers.userGuessedNumber === randomNumber) {
+  console.log("congratulations! you guessed right number.");
+} else {
+  console.log(" you guessed wrong number. 'Try Again'");
+}
+
+console.log("THE END");
