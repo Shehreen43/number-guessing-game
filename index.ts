30112edc3b99494c68e8a5005fa341e5a9b08f0b
Shehreen <160480886+Shehreen43@users.noreply.github.com>
2024-03-31 11:04:21 +0500

    now i add commit

--- index.ts
+++ index.ts
@@ -2,7 +2,11 @@
 
 import inquirer from "inquirer";
 
-//1) computer will generate a random num
+//1) computer will generate a random num.
+
+//2) user input  for guessing number.
+
+//3) computer compare user input with coputer genrated number and show result
 
 const randomNumber = Math.floor(Math.random() * 10 + 1);
 
