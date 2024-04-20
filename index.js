4293e248f4dc25e6de8bb58b6df589b82f3c6d35
Shehreen <160480886+Shehreen43@users.noreply.github.com>
2024-04-20 21:04:50 +0500

    ....

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-//1) computer will generate a random num
+//1) computer will generate a random num.
+//2) user input  for guessing number.
+//3) computer compare user input with coputer genrated number and show result
 const randomNumber = Math.floor(Math.random() * 10 + 1);
 const answers = await inquirer.prompt([
     {
