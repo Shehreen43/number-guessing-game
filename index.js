119cbda45e2cedac0c2af7b46080190ddeb61e2e
Shehreen <160480886+Shehreen43@users.noreply.github.com>
2024-05-19 10:55:20 +0500

    ....

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-#! /usr/bin/env node
+#! /usr/bin/evn node
 import inquirer from "inquirer";
+import chalk from "chalk";
 //1) computer will generate a random num.
 //2) user input  for guessing number.
 //3) computer compare user input with coputer genrated number and show result
+console.log(chalk.italic.cyan(` ***_WELLCOM TO MY " NUMBER GUESS GAME "_*** `));
 const randomNumber = Math.floor(Math.random() * 10 + 1);
 const answers = await inquirer.prompt([
     {
@@ -12,9 +14,9 @@ const answers = await inquirer.prompt([
     },
 ]);
 if (answers.userGuessedNumber === randomNumber) {
-    console.log("congratulations! you guessed right number.");
+    console.log(chalk.italic.magenta("congratulations! you guessed right number."));
 }
 else {
-    console.log(" you guessed wrong number. 'Try Again'");
+    console.log(chalk.dim.red(" you guessed wrong number. 'Try Again'"));
 }
-console.log("THE END");
+console.log(chalk.italic.yellowBright(" =======>THE END<======= "));
